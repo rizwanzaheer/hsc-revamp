94754358bd0aee32e2430c2de837592e806a6be9
Rizwan Zaheer <rizwan@hoo.com>
2022-01-24 16:34:18 +0400

    Integration of ElementPlus-UI with tailwindcss Example of Elementplus-ui with tailwindcss

--- src/main.js
+++ src/main.js
@@ -1,7 +1,10 @@
 import { createApp } from 'vue'
+import ElementPlus from 'element-plus'
+import 'element-plus/dist/index.css'
+
 import App from './App.vue'
 import router from './router'
 import store from './store'
 import './index.css'
 
-createApp(App).use(router).use(store).mount('#app')
+createApp(App).use(ElementPlus).use(router).use(store).mount('#app')
