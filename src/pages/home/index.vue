02ff2a668122b89de5b516f6009a31cd28cad49c
Rizwan Zaheer <rizwan@hoo.com>
2022-01-29 10:09:59 +0400

    Add the main banner update the logo Add the banner section etc

--- src/pages/home/index.vue
+++ src/pages/home/index.vue
@@ -1,3 +1,11 @@
 <template>
-    <h1>Dynamic home page loading here!</h1>
+    <div class="home-con">
+        <!-- <h1>Dynamic home page loading here!</h1> -->
+        <div class="banner-con">
+            <el-button
+                type="text"
+                class="uppercase text-[#E2459A] hover:text-[#02ead0]"
+            >New Journey ---</el-button>
+        </div>
+    </div>
 </template>
