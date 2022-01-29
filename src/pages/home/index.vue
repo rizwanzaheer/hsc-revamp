339f13b9779eded5840bde2145a48b7e1f657633
Rizwan Zaheer <rizwan@hoo.com>
2022-01-29 15:59:34 +0400

    complete the counter sections

--- src/pages/home/index.vue
+++ src/pages/home/index.vue
@@ -44,11 +44,42 @@
                 </el-link>
             </div>
 
-            <!-- <el-row>
-                <el-col :span="24" :gutter="20">
-                    <div class="grid-content bg-purple-dark">tetsdfad</div>
+            <el-row :gutter="20" justify="center" class="mt-[70px]">
+                <el-col :lg="4">
+                    <div class="grid-content">
+                        <h1 class="text-white text-[35px]">181M</h1>
+                        <h5 class="text-[#02EAD0] text-[12px] uppercase">Total value locked (USDT)</h5>
+                    </div>
                 </el-col>
-            </el-row>-->
+                <span class="border-l-2 border-white w-[25px] h-[25px] ml-2 mt-[25px]"></span>
+                <el-col :lg="4">
+                    <div class="grid-content">
+                        <h1 class="text-white text-[35px]">25,425</h1>
+                        <h5 class="text-[#02EAD0] text-[12px] uppercase">Number of transactions</h5>
+                    </div>
+                </el-col>
+                <span class="border-l-2 border-white w-[25px] h-[25px] ml-2 mt-[25px]"></span>
+                <el-col :lg="4">
+                    <div class="grid-content">
+                        <h1 class="text-white text-[35px]">126</h1>
+                        <h5 class="text-[#02EAD0] text-[12px] uppercase">transaction per second</h5>
+                    </div>
+                </el-col>
+                <span class="border-l-2 border-white w-[25px] h-[25px] ml-2 mt-[25px]"></span>
+                <el-col :lg="4">
+                    <div class="grid-content">
+                        <h1 class="text-white text-[35px]">65,098</h1>
+                        <h5 class="text-[#02EAD0] text-[12px] uppercase">Active addresses</h5>
+                    </div>
+                </el-col>
+                <span class="border-l-2 border-white w-[25px] h-[25px] ml-2 mt-[25px]"></span>
+                <el-col :lg="4">
+                    <div class="grid-content">
+                        <h1 class="text-white text-[35px]">27</h1>
+                        <h5 class="text-[#02EAD0] text-[12px] uppercase">Active Projects</h5>
+                    </div>
+                </el-col>
+            </el-row>
         </div>
     </el-main>
 </template>
@@ -56,7 +87,6 @@
 import CustomButton from '../../components/CustomButton.vue';
 import { ArrowRight } from '@element-plus/icons-vue';
 </script>
-
 
 <style lang="scss">
 @import '../../sass/common/_var.scss';
@@ -96,4 +126,30 @@ import { ArrowRight } from '@element-plus/icons-vue';
 //     //     cursor: pointer;
 //     // }
 // }
+.el-row {
+    margin-bottom: 20px;
+    &:last-child {
+        margin-bottom: 0;
+    }
+}
+.el-col {
+    border-radius: 4px;
+}
+.bg-purple-dark {
+    background: #99a9bf;
+}
+.bg-purple {
+    background: #d3dce6;
+}
+.bg-purple-light {
+    background: #e5e9f2;
+}
+.grid-content {
+    border-radius: 4px;
+    min-height: 36px;
+}
+.row-bg {
+    padding: 10px 0;
+    background-color: #f9fafc;
+}
 </style>
