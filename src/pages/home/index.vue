d920543dfc3f5534499b11370c289e99811a18f2
Rizwan Zaheer <rizwan@hoo.com>
2022-01-29 10:35:15 +0400

    Remove demo font size file Add the main layout Add montserrat font family using import link of google fonts also add in reset font family css

--- src/pages/home/index.vue
+++ src/pages/home/index.vue
@@ -1,11 +1,19 @@
 <template>
-    <div class="home-con">
-        <!-- <h1>Dynamic home page loading here!</h1> -->
-        <div class="banner-con">
-            <el-button
-                type="text"
-                class="uppercase text-[#E2459A] hover:text-[#02ead0]"
-            >New Journey ---</el-button>
+    <el-main>
+        <div class="home-con">
+            <!-- <h1>Dynamic home page loading here!</h1> -->
+            <div class="banner-con">
+                <el-button
+                    type="text"
+                    class="uppercase text-[#E2459A] hover:text-[#02ead0] text-lg"
+                >New Journey ---</el-button>
+            </div>
+
+            <el-row>
+                <el-col :span="24" :gutter="20">
+                    <div class="grid-content bg-purple-dark">tetsdfad</div>
+                </el-col>
+            </el-row>
         </div>
-    </div>
+    </el-main>
 </template>
