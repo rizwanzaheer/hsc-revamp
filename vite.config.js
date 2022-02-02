0e62ea85d84a18ce8b01b0ee66c3a61cdd705aeb
Rizwan Zaheer <rizwan@hoo.com>
2022-02-02 11:05:57 +0400

    add script folder/script for the github deployment, update the base url, new build

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import vue from '@vitejs/plugin-vue';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-    base: '/hsc-revamp/',
+    base: process.env.NODE_ENV === 'production' ? '/hsc-revamp/' : '/',
     plugins: [vue()],
 });
