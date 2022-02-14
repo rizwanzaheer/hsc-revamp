6f254b900236df48ccfa6810043667c857e77d3c
Rizwan Zaheer <rizwan@hoo.com>
2022-02-14 12:33:48 +0400

    build latest version of the app

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,6 @@ import vue from '@vitejs/plugin-vue';
 // https://vitejs.dev/config/
 export default defineConfig({
     // base: process.env.NODE_ENV === 'production' ? '/hsc-revamp/' : '/', //for the gitpages only
-    base: '/',
+    base: '/', // for the staging/prod
     plugins: [vue()],
 });
