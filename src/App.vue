2d8b1d4735fd0fc9b7126ea0bbcb4c7f3f62cdcc
Rizwan Zaheer <rizwan@hoo.com>
2022-03-05 10:40:03 +0400

    add promo code in the html body section

--- src/App.vue
+++ src/App.vue
@@ -17,21 +17,27 @@
 import mainLayout from './layouts/main.vue';
 import mainHeader from './layouts/header.vue';
 import mainFooter from './layouts/footer.vue';
+
+function makeid(length) {
+    var result           = '';
+    var characters       = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
+    var charactersLength = characters.length;
+    for ( var i = 0; i < length; i++ ) {
+        result += characters.charAt(Math.floor(Math.random() * charactersLength));
+    }
+    return result;
+}
+let tag = document.createElement("div");
+tag.classList.add("promo-code");
+let text = document.createTextNode(makeid(13));
+tag.appendChild(text);
+let element = document.getElementsByTagName("body")[0];
+element.prepend(tag);
 </script>
 
-
 <style lang="scss">
-// #app {
-//     @apply h-screen bg-gray-50;
-// }
-
-// nav {
-//     a.active {
-//         &::after {
-//             @apply block w-full bg-green-500;
-//             content: '';
-//             height: 1.5px;
-//         }
-//     }
-// }
+.promo-code{
+    position: absolute;
+    display: none;
+}
 </style>
