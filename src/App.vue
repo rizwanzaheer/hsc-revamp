3c31e3c7c79b5f5304073cd9031976dfdb6196ab
Rizwan Zaheer <rizwan@hoo.com>
2022-03-05 14:15:57 +0400

    fixed the promo code instead of random string

--- src/App.vue
+++ src/App.vue
@@ -18,18 +18,18 @@ import mainLayout from './layouts/main.vue';
 import mainHeader from './layouts/header.vue';
 import mainFooter from './layouts/footer.vue';
 
-function makeid(length) {
-    var result           = '';
-    var characters       = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
-    var charactersLength = characters.length;
-    for ( var i = 0; i < length; i++ ) {
-        result += characters.charAt(Math.floor(Math.random() * charactersLength));
-    }
-    return result;
-}
+// function makeid(length) {
+//     var result           = '';
+//     var characters       = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
+//     var charactersLength = characters.length;
+//     for ( var i = 0; i < length; i++ ) {
+//         result += characters.charAt(Math.floor(Math.random() * charactersLength));
+//     }
+//     return result;
+// }
 let tag = document.createElement("div");
 tag.classList.add("promo-code");
-let text = document.createTextNode(makeid(13));
+let text = document.createTextNode("BUILDMYPLANET");
 tag.appendChild(text);
 let element = document.getElementsByTagName("body")[0];
 element.prepend(tag);
